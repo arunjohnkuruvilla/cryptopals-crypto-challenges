a4ad1928d2d8d9ecfbe33b2abb918742a5492964
Arun Kuruvilla <arunjohnkuruvilla@gmail.com>
2023-03-17 11:37:49 -0400

    Set 1 Challenge 5 implementation

---
+++ challenges/set_1_challenge_5.py
@@ -0,0 +1,29 @@
+import sys
+import base64
+import binascii
+
+INPUT_STRING = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+
+OUTPUT_STRING = b'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+
+def repeated_xor_encrypt(plaintext, key):
+	key_length = len(key)
+	plaintext_length = len(plaintext)
+
+	ciphertext = b''
+
+	for counter, char in enumerate(plaintext):
+		current_key_char = key[counter%key_length]
+
+		current_ciphertext_char = chr(ord(char) ^ ord(current_key_char))
+
+		ciphertext = ciphertext + binascii.hexlify(current_ciphertext_char.encode())
+
+	return ciphertext
+
+if __name__ == '__main__':
+	ciphertext = repeated_xor_encrypt(INPUT_STRING, "ICE")
+
+	print(ciphertext)
+
+	print(ciphertext == OUTPUT_STRING)
