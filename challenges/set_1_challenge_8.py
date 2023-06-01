c83e8e1b4c8c95d0e4acc0cb2299efe88aaf5e5e
Arun Kuruvilla <arunjohnkuruvilla@gmail.com>
2023-06-01 14:35:05 -0400

    Set 1 Challenge 8 implementation

---
+++ challenges/set_1_challenge_8.py
@@ -0,0 +1,32 @@
+import binascii
+
+def detect_ecb_encryption(string, block_size = 16):
+	blocks = [string[i:i+block_size] for i in range(0, len(string), block_size)]
+
+	blocks_without_duplicates = [*set(blocks)]
+
+	if len(blocks_without_duplicates) < len(blocks):
+		return True
+	else:
+		return False
+
+def main():	
+	ciphertext_file = open("challenges/files/8.txt")
+
+	ciphertext_file_contents = ciphertext_file.readlines()
+
+	lineNumber = 1
+
+	for line in ciphertext_file_contents:
+		raw_ciphertext_line = binascii.unhexlify(line.rstrip())
+
+		status = detect_ecb_encryption(raw_ciphertext_line)
+
+		if status:
+			print(lineNumber)
+			break
+
+		lineNumber += 1
+
+if __name__ == '__main__':
+	main()
