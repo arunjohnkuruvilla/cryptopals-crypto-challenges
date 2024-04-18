eca3560da2396e40a81081c94484126c29fa7500
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2024-04-17 23:43:12 -0400

    Refactored Set 1 Challenge 1

--- challenges/set_1_challenge_01.py
+++ challenges/set_1_challenge_01.py
@@ -1,13 +1,12 @@
 import base64
 import binascii
 
-INPUT_STRING = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-
-OUTPUT_STRING = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-
 def get_base64_string_from_hex_encoded_string(hex_encoded_input):
 	return base64.b64encode(binascii.unhexlify(hex_encoded_input))
 
-print(get_base64_string_from_hex_encoded_string(INPUT_STRING))
+if __name__ == "__main__":
+	INPUT_STRING = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-print(OUTPUT_STRING.encode('ASCII') == get_base64_string_from_hex_encoded_string(INPUT_STRING))
+	OUTPUT_STRING = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+	assert OUTPUT_STRING.encode("ascii") == get_base64_string_from_hex_encoded_string(INPUT_STRING)
