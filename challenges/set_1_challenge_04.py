51d257df7fb3b4df82059b9e5bc677be2aeedcc9
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2024-04-21 11:46:19 -0400

    Refactored Set 1 Challenge 4

--- challenges/set_1_challenge_04.py
+++ challenges/set_1_challenge_04.py
@@ -1,24 +1,28 @@
+import binascii
 import set_1_challenge_03 as challenge_3
 
-file = open("./challenges/files/4.txt", "r")
-lines = file.readlines()
+def identify_single_char_xor():
+	source_file = open("./challenges/files/4.txt", "r")
+	lines = source_file.readlines()
 
-scores = {}
-keys = {}
-results = {}
+	max_line = 0
+	max_score = 0.0
+	max_key = ''
+	max_plaintext = ''
 
-for counter, line in enumerate(lines):
-	
+	for counter, line in enumerate(lines):
+		ciphertext_line = line.rstrip()
+		raw_ciphertext_line = binascii.unhexlify(ciphertext_line)
 
-	key, plaintext, score = challenge_3.detect_xor_key(line.strip())
+		current_key, current_plaintext, current_score = challenge_3.detect_xor_key(raw_ciphertext_line)
 
-	scores[counter] = score
-	keys[counter] = key
-	results[counter] = plaintext
+		if current_score > max_score:
+			max_score = current_score
+			max_plaintext = current_plaintext
+			max_key = current_key
+			max_line = counter
 
-sorted_scores = dict(sorted(scores.items(), key=lambda item: item[1])[:1])
+	print("Line #" + str(max_line) + " : Key '" + max_key + "' : " + max_plaintext)
 
-for key in sorted_scores.keys():
-	print("Line " + str(key+1) + ": " + results[key])
-
-
+if __name__ == '__main__':
+	identify_single_char_xor()
